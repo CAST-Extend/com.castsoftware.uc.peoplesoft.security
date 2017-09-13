72b8150af06f3c871158875e24df948bdff42723
kragg77 <JGD@corp.castsoftware.com>
2017-09-13 16:51:47 -0400

    Revert "1st functional Version"
    This reverts commit 4a6588b5273e4f6dee894988c94027f3a95d4236.
    
    # Conflicts:
    #	.checkstyle
    #	.pmd
    #	.project
    #	.pydevproject
    #	__pycache__/application_level.cpython-34.pyc
    #	__pycache__/cast_upgrade_1_5_7.cpython-34.pyc
    #	cast_upgrade_1_5_7.py
    #	com.castsoftware.uc.peoplesoft.security/cast_upgrade_1_5_6.py
    #	com.castsoftware.uc.peoplesoft.security/cast_upgrade_1_5_7.py
    #	lib_cast_upgrade_1_5_7.zip
    #	tests/test_application_level.py
    #	tests/test_regex.py

--- tests/test_application_level.py
+++ tests/test_application_level.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:tests/test_application_level.py
 '''
 @author: AGR
 '''
@@ -12,3 +13,19 @@ class Test(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+=======
+'''
+@author: AGR
+'''
+import unittest
+from cast.application.test import run
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        run(kb_name='psft_cwe2_local', application_name='Psft_CWE2_TC1')
+
+if __name__ == "__main__":
+    unittest.main()
+>>>>>>> parent of 4a6588b... 1st functional Version:com.castsoftware.uc.peoplesoft.security/tests/test_application_level.py
